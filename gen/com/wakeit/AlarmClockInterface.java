aed73588a03fb7f826dd2f45dd62565c539ed386
ido4848 <ido.aizenbud@gmail.com>
2014-05-06 22:56:36 +0300

    commit that should not happen

--- gen/com/wakeit/AlarmClockInterface.java
+++ gen/com/wakeit/AlarmClockInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: C:\\xampp\\htdocs\\WakeIt\\src\\com\\wakeit\\AlarmClockInterface.aidl
+ * Original file: C:\\Users\\Ido\\git\\WakeIt\\src\\com\\wakeit\\AlarmClockInterface.aidl
  */
 package com.wakeit;
 public interface AlarmClockInterface extends android.os.IInterface
